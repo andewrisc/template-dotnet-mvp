d9a7b693428400a56dd43bb93391d80e7b4b9c62
AndewRisc <zendy.dev@yahoo.com>
2025-07-27 23:33:43 +0700

    modify i user repository

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -6,8 +6,6 @@ namespace API.Interfaces;
 public interface IUserRepository : IBaseRepository<User>
 {
     Task<User?> GetCurrentUserId(int Id);
-
     Task<User?> GetByEmailAndPasswordAsync(string Email, string Password);
-
     Task<User?> GetByEmailAsync(string email);
 }
